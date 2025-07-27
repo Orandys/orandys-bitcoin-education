18a232256405a57f95cecb204bfc0ecd187cfc46
Orandys <orandys.bitcoin@gmail.com>
2025-07-27 19:02:51 +0200

    Delete orandys-bitcoin-education directory

--- orandys-bitcoin-education/components/utils/navigation-helpers.ts
+++
@@ -1,8 +0,0 @@
-export const getShortTitle = (title: string) => {
-  if (title.includes("Introduction")) return "Introduction";
-  if (title.includes("techniques")) return "Technique";
-  if (title.includes("sécurité")) return "Sécurité";
-  if (title.includes("Économie")) return "Économie";
-  if (title.includes("Applications")) return "Applications";
-  return title.split(' ')[0];
-};
